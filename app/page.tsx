5ae77a54e6c1e91b19baacf98656eecc67864a41
Arpit Rulania <rarpit124@gmail.com>
2024-05-20 16:39:42 +0530

    added auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Arpit!</h1>
-      <h1 className="h2-bold">Arpit!</h1>
-      <h1 className="h3-bold">Arpit!</h1>
-    </div>
-  );
-}
